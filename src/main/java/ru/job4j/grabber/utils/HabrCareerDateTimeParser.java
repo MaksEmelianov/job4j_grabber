c49188fe8f40a149e015b58f3b4af72e337a2116
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-12-07 19:20:18 +0500

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/HabrCareerDateTimeParser.java
@@ -1,11 +1,12 @@
 package ru.job4j.grabber.utils;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class HabrCareerDateTimeParser implements DateTimeParser {
     @Override
     public LocalDateTime parse(String parse) {
-        return LocalDateTime.parse(parse.split("\\+")[0]);
+        return ZonedDateTime.parse(parse, DateTimeFormatter.ISO_DATE_TIME).toLocalDateTime();
     }
 }
