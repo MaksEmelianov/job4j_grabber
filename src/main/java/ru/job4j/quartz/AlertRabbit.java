83766918a224cba84ee11eea2a65994eee119bcb
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-12-07 13:58:24 +0500

    1.1. Job c параметрами [#260360]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -5,6 +5,10 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -13,22 +17,41 @@ import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
 
+    private static final Properties CONFIG = getConfig();
+
     public static void main(String[] args) {
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
-            JobDetail job = newJob(Rabbit.class).build();
+            JobDataMap data = new JobDataMap();
+            data.put("store", getConnect());
+            JobDetail job = newJob(Rabbit.class)
+                    .usingJobData(data)
+                    .build();
             SimpleScheduleBuilder timer = simpleSchedule()
-                    .withIntervalInSeconds(Integer.parseInt(getConfig().getProperty("rabbit.interval")))
+                    .withIntervalInSeconds(Integer.parseInt(CONFIG.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
                     .withSchedule(timer)
                     .build();
             scheduler.scheduleJob(job, trigger);
-        } catch (SchedulerException e) {
+            Thread.sleep(10000);
+            scheduler.shutdown();
+        } catch (SchedulerException
+                 | InterruptedException
+                 | ClassNotFoundException
+                 | SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static Connection getConnect() throws ClassNotFoundException, SQLException {
+        Class.forName(CONFIG.getProperty("driver_class"));
+        return DriverManager.getConnection(
+                        CONFIG.getProperty("url"),
+                        CONFIG.getProperty("username"),
+                        CONFIG.getProperty("password"));
     }
 
     private static Properties getConfig() {
@@ -44,8 +67,16 @@ public class AlertRabbit {
 
     public static class Rabbit implements Job {
         @Override
-        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        public void execute(JobExecutionContext context) {
             System.out.println("Rabbit runs here ...");
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("store");
+            try (PreparedStatement statement = connection
+                    .prepareStatement("insert into rabbit(created_date) values (?)")) {
+                statement.setTimestamp(1, Timestamp.valueOf(LocalDateTime.now()));
+                statement.execute();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
