ebf8b491814215d078fbe960c79d93daeb349911
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2022-12-09 15:19:29 +0500

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -10,6 +10,10 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Properties;
 
@@ -78,10 +82,32 @@ public class Grabber implements Grab {
             Store store = grabber.store();
             Parse parse = grabber.parse();
             grabber.init(parse, store, scheduler);
+            grabber.web(store);
         } catch (IOException ioException) {
             throw new IllegalArgumentException("Error in getting config");
         } catch (SchedulerException e) {
             throw new IllegalArgumentException("Error in running scheduler");
         }
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(config.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
